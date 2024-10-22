7664403c04a01e3733bd5003b78a08b196e217eb
Yash Pandey <pandeyash1706@gmail.com>
2024-10-22 19:01:44 +0530

    isAnagram code added

---
+++ src/isanagram.java
@@ -0,0 +1,21 @@
+public class isanagram {
+    public static boolean isAnagram(String s , String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        for(int val : count){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
